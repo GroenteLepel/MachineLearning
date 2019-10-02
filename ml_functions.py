51f695bd8d175c92d894226f72e689f33dce5aa6
Ludo van Alst <l.vanalst@student.science.ru.nl>
2019-10-02 10:15:31 +0200

    wrote classification function, changed weight initialization

--- ml_functions.py
+++ ml_functions.py
@@ -17,10 +17,10 @@ def probability(x, w):
     return sigmoid(x.dot(w))
 
 
-def loss_function(weight, coords, label, decay_factor):
+def loss_function(weights, coords, label, decay_factor):
     """
     Loss function which is the goal to be minimized.
-    :param weight: weights connected to each x value
+    :param weights: weights connected to each x value
     :param coords: N-dimensional coordinates for each data point
     :param label: label given to each data point with coordinates "coords", is
     either 0 or 1.
@@ -28,10 +28,10 @@ def loss_function(weight, coords, label, decay_factor):
     implemented in the determination of finding the minimum.
     :return:
     """
-    y = probability(coords, weight)
+    y = probability(coords, weights)
     n_points = len(label)  # amount of data points
     if decay_factor != 0.0:
-        decay_term = decay_factor * np.sum(weight ** 2) / (2 * len(weight))
+        decay_term = decay_factor * np.sum(weights ** 2) / (2 * len(weights))
     else:
         decay_term = 0.0
     return float(
@@ -42,7 +42,7 @@ def loss_function(weight, coords, label, decay_factor):
     )
 
 
-def gradient_function(weight, coords, label, decay_factor):
+def gradient_function(weights, coords, label, decay_factor):
     """
     Gradient (derivative) of the loss_function with respect to the weight for
     each data point (w_i). This is used for determining the direction towards
@@ -57,13 +57,13 @@ def gradient_function(weight, coords, label, decay_factor):
     :return:
     """
     n_points = len(label)  # amount of data points
-    diff = (probability(coords, weight) - label)
+    diff = (probability(coords, weights) - label)
     return np.transpose(1. / n_points * np.transpose(diff).dot(coords)) \
-           + decay_factor * weight / len(weight)
+           + decay_factor * weights / len(weights)
 
 
 # TODO: implement
-def hessian(weight, coords, label, decay_factor):
+def hessian(weights, coords, label, decay_factor):
     """
     Hessian information used to optimize finding a minimum in E
     (loss_function). By making a quadratic approximation to E around w, the
@@ -79,12 +79,24 @@ def hessian(weight, coords, label, decay_factor):
     """
     n_points = len(label)
     if decay_factor != 0.0:
-        decay_term = np.identity(n_points) * decay_factor * 1. / len(weight)
+        decay_term = np.identity(n_points) * decay_factor * 1. / len(weights)
     else:
         decay_term = 0.0
-    y = probability(coords, weight)
+    y = probability(coords, weights)
     return 1. / n_points * (
         np.transpose(coords).dot(((1 - y) * y * coords))) + decay_term
+            
+
+def classification_check(coords, labels, weights):
+    """
+    :coords: pixels intensities
+    :labels: denotes 0 or 1 for classification of the datapoint
+    :return: percentages of wrongly classified data (for training and test)
+    """
+    probabilities = probability(coords, weights)
+    prob_bools = np.round(probabilities)
+    equal_counter = (prob_bools == labels).sum()
+    return 1- (equal_counter / len(labels))
 
 
 def gradient_descent(train_coords, train_labels, test_coords, test_labels,
@@ -117,7 +129,7 @@ def gradient_descent(train_coords, train_labels, test_coords, test_labels,
     """
     # initializing constants
     dw = np.zeros((784, 1))
-    w = np.random.uniform(-0.01, 0.01, (784, 1))
+    w = np.random.normal(0, 1./np.sqrt(784), (784, 1))
     train_loss = np.zeros(epochs)
     test_loss = np.zeros(epochs)
     print('Starting gradient descent using eta={0:4.2f}, alpha={1:4.2f}, '
@@ -141,4 +153,4 @@ def gradient_descent(train_coords, train_labels, test_coords, test_labels,
 
     print('done in {0:3.2f} seconds'.format(time.time() - time_start))
 
-    return train_loss, test_loss
+    return train_loss, test_loss, w
