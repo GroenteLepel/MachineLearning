4078522a3291ee9c7435707098da3eed063d1402
Laurens Sluyterman <laurens@ip-145-116-135-219.wlan-int.ru.nl>
2019-10-16 12:52:46 +0200

    pauper lelijk yo

--- week6b.py
+++ week6b.py
@@ -47,7 +47,7 @@ pi1=0.5
 pi2=0.5
 r1=np.zeros(len(X))
 r2=np.zeros(len(X))
-Niterations=5
+Niterations=1
 for j in range(0,Niterations):
     Snew1=np.array([[0,0],[0,0]])
     Snew2=np.array([[0,0],[0,0]])
@@ -60,13 +60,27 @@ for j in range(0,Niterations):
     mu1[1]=np.sum(r1*Y)/np.sum(r1)
     mu2[0]=np.sum(r2*X)/np.sum(r2)
     mu2[1]=np.sum(r2*Y)/np.sum(r2)
-    S1=Snew1
-    S2=Snew2
+    S1=Snew1/np.sum(r1)
+    S2=Snew2/np.sum(r1)
     pi1=np.mean(r1)
     pi2=np.mean(r2)
     print('step',j+1)
-    plt.scatter(X,Y,cmap='seismic',c=r1)
-    plt.show()
+    fig = plt.figure()
+    ax = fig.add_subplot(111, aspect='auto')
+    w,v=np.linalg.eig(S1)
+    w2,v2=np.linalg.eig(S2)
+    index = np.where(w == np.max(w))
+    index2 = np.where(w2 == np.max(w2))
+    ori=np.arctan(v[1][index]/v[0][index])
+    ori2=np.arctan(v2[1][index]/v2[0][index])
+    major,minor=2*np.sqrt(5.991*w[0]),2*np.sqrt(5.991*w[1])
+    major2,minor2=2*np.sqrt(5.991*w2[0]),2*np.sqrt(5.991*w2[1])
+    ellipse = Ellipse(mu1,minor,major,angle=ori,alpha=0.1,color='r')
+    ellipse2 = Ellipse(mu2,minor2,major2,angle=ori2,alpha=0.1,color='b')
+    ax.add_patch(ellipse)
+    ax.add_patch(ellipse2)
+    ax.scatter(X,Y,cmap='seismic',c=r1)
+    fig.show()
 
 
 
@@ -74,10 +88,16 @@ for j in range(0,Niterations):
 fig = plt.figure()
 ax = fig.add_subplot(111, aspect='auto')
 w,v=np.linalg.eig(S1)
+w2,v2=np.linalg.eig(S2)
 index = np.where(w == np.max(w))
-alpha=np.arctan(v[1][index]/v[0][index])
+index2 = np.where(w2 == np.max(w2))
+ori=np.arctan(v[1][index]/v[0][index])
+ori2=np.arctan(v2[1][index]/v2[0][index])
 major,minor=2*np.sqrt(5.991*w[0]),2*np.sqrt(5.991*w[1])
-ellipse = Ellipse(mu1,minor,major,angle=alpha)
+major2,minor2=2*np.sqrt(5.991*w2[0]),2*np.sqrt(5.991*w2[1])
+ellipse = Ellipse(mu1,minor,major,angle=ori,alpha=0.1,color='r')
+ellipse2 = Ellipse(mu2,minor2,major2,angle=ori2,alpha=0.1,color='b')
 ax.add_patch(ellipse)
+ax.add_patch(ellipse2)
 ax.scatter(X,Y,cmap='seismic',c=r1)
 fig.show()
