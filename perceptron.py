6835115093404c071078022847c823757980d9d5
Daniel <d.kok@student.science.ru.nl>
2019-10-24 11:52:13 +0200

    Small layout changes.

--- perceptron.py
+++ perceptron.py
@@ -1,18 +1,19 @@
 # Adapted from the code on https://www.tensorflow.org/tutorials/images/cnn 
 from __future__ import absolute_import, division, print_function, unicode_literals
-
-import tensorflow as tf
-
 from tensorflow.keras import datasets, layers, models, regularizers
 
+# Download and prepare the CIFAR10 dataset
 (train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
-
 
 # Normalize pixel values to be between 0 and 1
 train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Create the convolutional base
 model = models.Sequential([
     layers.Flatten(),
-    layers.Dense(10, activation='softmax')])
+    layers.Dense(10, activation='softmax')
+])
+
 model.compile(optimizer='adam',
               loss='sparse_categorical_crossentropy',
               metrics=['accuracy'])
