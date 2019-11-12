cf1a79623fd0d5c90326b1891ad7bf2636b50f43
Daniël Kok <d.kok@student.science.ru.nl>
2019-11-12 14:26:36 +0100

    Added exercise description and tips from Alex.

---
+++ GibbsSampling.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+# Implement Gibbs sampling for the inference of a single one-dimensional
+# Gaussian which we studied using maximuml ikelihood in section22.1. Assign a
+# broad Gaussian prior toand a broad gamma prior (24.2) to the precision
+# parameter= 1=2. Each update ofwill involve a sample from a Gaussian
+# distribution,and each update ofrequires a sample from a gammad istribution.
+
+# Sample from a 1D Gaussian
+# The variance and mean need to be sampled. The variance is independent of the
+# mean. The mean is dependent of your variance! With very small variance, the
+# area of where your mean can be is much smaller.
+
+# You sample the mean based on two conditionals:
+#  for i in range(__):
+#       x_i = p_1(__|__)
+#       T_i = p_2(__|__)
+
+# You use the mean and variance to
+
+# Derive the conditionals from the first section on the wiki-page.
