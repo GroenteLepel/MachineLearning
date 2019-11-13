08c32aeb41eea437736c9c8b637ffc73421f80df
dkok <dkok@student.science.ru.nl>
2019-11-13 12:16:34 +0100

    Added base function an small start to exercise.

--- ImportanceSampling.py
+++ ImportanceSampling.py
@@ -1,3 +1,7 @@
+#%% Imports
+import numpy as np
+
+#%% Exercise description
 # Have a gaussian for P(x) and Q(x) both with means 0 and stdevs std_p, std_q.
 #  Sample x_q from Q(x), calculate the probability P(x_q), and use this to:
 #
@@ -11,3 +15,23 @@
 
 # The plots seem to run on the x-axis from 1 to 1.6, and contain runs with
 #  R = 1.000, 10.000, 100.000.
+
+#%% Function declerations
+def prob_in_gaussian(x):
+    probability = 
+    return probability
+
+def normal_pdf(x, mean, stdev):
+    var = float(stdev)**2
+    denom = (2 * np.pi * var) ** (1./2)
+    num = np.exp(-(float(x) - float(mean)) ** 2 / (2 * var))
+    return num/denom
+
+#%% Init
+mean_p, mean_q = 0, 0
+stdev_p, stdev_q = 1, 2
+n_points = int(1e3)
+
+
+#%% 
+x_q = np.random.normal(loc=mean_q, scale=stdev_q, size=n_points)
