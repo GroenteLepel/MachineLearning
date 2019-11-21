2fb0f7c2bd779eddd5c8688e51ff4becfe200e3d
Daniël Kok <d.kok@student.science.ru.nl>
2019-11-21 12:36:45 +0100

    Added script from internet for comparison and modification.

--- metropolis_hastings.py
+++ metropolis_hastings.py
@@ -1,9 +1,25 @@
+import numpy as np
+
+
 def metropolis_hastings(p):
     """
     Method for sampling data from a distribution p
     :param p: distribution to sample using this method
     :return:
     """
+    # copy- pasted from interwebs
+    # ===========================
+    x, y = 0., 0.
+    samples = np.zeros(2)
+
+    x_star, y_star = np.array([x, y]) + np.random.normal(size=2)
+    if np.random.rand() < p(x_star, y_star) / p(x, y):
+        x, y = x_star, y_star
+    samples[i] = np.array([x, y])
+
+    return samples
+    # ============================
+    # end copy paste
 
 
 def acceptance(p, q, sample_x, current_state):
