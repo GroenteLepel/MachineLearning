a85b763dfd5155ccf0eacc89af23f28be382e39a
Daniël Kok <d.kok@student.science.ru.nl>
2019-11-21 12:47:45 +0100

    Template file for the hamilton monte carlo sampling method.

---
+++ hamilton_mc.py
@@ -0,0 +1,8 @@
+def hamilton_mc(p):
+    """
+    Method of sampling data given a distribution p via the hamilton monte carlo
+    method.
+    :param p: distribution to sample.
+    :return:
+    """
+    return 0
