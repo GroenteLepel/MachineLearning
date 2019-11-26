fixed minus sign

mcmc_exercise.py
@@ -28,7 +28,7 @@ t = np.array([0, 0, 0, 0, 0, 1, 1, 1, 1, 1])
 # samples = metropolis_hastings(circle, n_dims=2, n_points=10000)
 
 samples = metropolis_hastings(functools.partial(p_star_distribution, x, t),
-                                n_points=100000)
+                                n_points=30000)
 
 sns.jointplot(samples[:, 0], samples[:, 1])
 plt.show()