650d08979371e7e8f4579070a8250a4bc1389af8
Daniël Kok <d.kok@student.science.ru.nl>
2019-11-26 12:24:51 +0100

    Tested on a circle distribution, works.

--- metropolis_hastings.py
+++ metropolis_hastings.py
@@ -1,12 +1,11 @@
 import numpy as np
+from scipy.stats import multivariate_normal
 
 
-def normal(x, mean):
+def normal(x, mean=0):
     """
-    :param x:
-    :param mean:
-    :param stdev:
-    :return: float, probability that x is in a gaussian.
+    :return: float, probability that x is in a 3D-gaussian with identity matrix
+    as covariance.
     """
 
     return np.exp(-1 * np.dot((x - mean), (x - mean)) / 2) / \
@@ -28,16 +27,14 @@ def metropolis_hastings(p, n_points=1000, n_dims=3):
     for i in range(n_points):
         if i % (n_points / 4) == 0:
             print(i)
-        new_x = x + np.random.normal(size=n_dims)
+        new_x = np.random.normal(size=n_dims)
 
         counter = 0
         while not accept(p, normal, new_x, x):
-            new_x = x + np.random.normal(size=n_dims)
+            new_x = np.random.normal(size=n_dims)
             counter += 1
-            if counter == 1000:
-                print('wtf')
-                print(new_x)
-                print(x)
+            if counter == 100000:
+                print("overflowing at", i)
 
         samples[i] = new_x
         x = new_x
@@ -47,17 +44,23 @@ def metropolis_hastings(p, n_points=1000, n_dims=3):
 
 def accept(p, q, sample_x, current_state):
     """
-    Calculate the accaptence ratio between sample_x x' and current_state x
+    Calculate the acceptance ratio between sample_x x' and current_state x
     according to the distributions p and q, and returns the proper parameter
     :param p: distribution to sample
     :param q: distribution picked by the metropolis_hastings method
-    :param sample_x:
-    :param current_state:
-    :return:
+    :param sample_x: newly sampled x-coordinate to check if it suffices to the
+    chosen distribution p
+    :param current_state: old sample which has been previously accepted by
+    the metropolis hastings algorithm.
+    :return: True or False value if the sample_x is accepted
     """
+    p_star = p(sample_x)
+    p_test = p(current_state)
+    q_test = q(0, sample_x)
+    q_star = q(sample_x, 0)
 
-    a = p(sample_x) * q(current_state, sample_x) / \
-        (p(current_state) * q(sample_x, current_state))
+    a = p(sample_x) * q(current_state) / \
+        (p(current_state) * q(sample_x))
 
     if a >= 1:
         return True
