4b5e967aadaf08148f47c506b79c18b8537b6287
Daniël Kok <d.kok@student.science.ru.nl>
2019-12-06 12:17:44 +0100

    Copy-pasted matlab code of exercise into our code, did some cleanup of unused code and made to-do list in simulated_annealing function.

--- ising_optimiser.py
+++ ising_optimiser.py
@@ -1,6 +1,4 @@
-import random
 from itertools import combinations
-
 import numpy as np
 
 """
@@ -12,46 +10,6 @@ Theory
   - how are II and SA related?
   - What are the advantages of each method? (or what do you expect)
 """
-
-def metropolis_hastings(n_points, n_dims=3):
-    """
-    Method for sampling data from a distribution p
-    :param p: distribution to sample using this method
-    :param n_points: amount of points you want to sample from distribution p
-    :param n_dims: amount of dimensions of the points which are sampled
-    :return: array with shape (n_points, n_dims)
-    """
-
-    samples = np.zeros((n_points, n_dims))
-    x = np.zeros(n_dims)
-
-    for i in range(n_points):
-        if i % (n_points / 4) == 0:
-            print(i)
-        new_x = x + np.random.normal(size=n_dims, scale=0.1)
-
-        counter = 0
-        if accept_exponent(objective_function, data, labels, new_x, x):
-            samples[i] = new_x
-            x = new_x
-        else:
-            samples[i] = x
-
-    return samples
-
-
-def accept_exponent(f, data, labels, sample_x, current_state):
-    # TODO: Remove this ugly specific implementation.
-    difference = f(sample_x, data, labels, 0.01) - f(current_state, data,
-                                                     labels, 0.01)
-    a = np.exp(- difference)
-
-    if a >= 1:
-        return True
-    elif np.random.rand() <= a:
-        return True
-    else:
-        return False
 
 
 class IsingOptimiser:
@@ -96,19 +54,24 @@ class IsingOptimiser:
     def flip_state(self, i):
         self.state[i] *= -1
 
+    def generate_flip_combinations(self, neighbourhood):
+        indices = np.arange(self.n)
+        np.random.shuffle(indices)
+        flip_combs_iterator = list(combinations(indices, neighbourhood))
+        flip_combinations = [np.array(x) for x in flip_combs_iterator]
+
+        return flip_combinations
+
     def ising_energy(self):
         return -0.5 * np.dot(self.state, np.dot(self.weights, self.state))
 
     def iterative_improvement(self, neighbourhood=3):
         old_cost = self.ising_energy()
-        indices = np.arange(self.n)
 
         for n_flips in range(1, neighbourhood + 1):
-            np.random.shuffle(indices)
-            flip_combs_dummy = list(combinations(indices, n_flips))
-            flip_combs = [np.array(x) for x in flip_combs_dummy]
+            flip_combinations = self.generate_flip_combinations(n_flips)
 
-            for flip_comb in flip_combs:
+            for flip_comb in flip_combinations:
                 self.flip_state(flip_comb)
                 if self.ising_energy() < old_cost:
                     return True
@@ -116,20 +79,97 @@ class IsingOptimiser:
 
         return False
 
-    def p_simulated_annealing(self, beta):
+    def energy_distribution(self, beta):
+        """
+        probability density (not normalised) to sample from using metropolis
+        hastings.
+        :param beta:
+        :return:
+        """
         return np.exp(-beta * self.ising_energy())
-    
-    def metropolis_hastings(self, beta, n_points):
-        samples = np.zeros(n_points)
-        
-        index = 0
-        while samples[-1] == 0:
-            new_sample = self._generate_spin_state()
-            
-            difference = p_simulated_annealing()
-            
-            index += 1
-        
 
-    def simulated_annealing(self, beta_init = 0.0091, markov_chain_length = 2000, n_betas = 450):
-        beta_list = 1.01 * np.logspace(1, n_betas, base = beta_init)    
+    # def metropolis_hastings(self, beta, n_points):
+    #     samples = np.zeros(n_points)
+    #
+    #     index = 0
+    #     while samples[-1] == 0:
+    #         new_sample = self._generate_spin_state()
+    #
+    #         difference = p_simulated_annealing()
+    #
+    #         index += 1
+
+    def estimate_max_energy_diff(self, neighbourhood=1):
+        """
+        Estimate the max energy difference of the ising model system based on
+        the amount of spins that are flipped.
+
+        It is called "estimate", so we figured how we would do it is as
+        follows:
+        - generate all possible combinations of spin flips in the max
+         neighbourhood, since this has the highest probability of containing
+         the largest energy difference (?)
+        - calculate the first i energies of these combinations
+        - pick the max difference
+
+        :param neighbourhood:
+        :return:
+        """
+        max_e_diff = 0
+        # TODO: implement this function.
+        return max_e_diff
+
+    def simulated_annealing(self, neighbourhood,
+                            length_markov_chain=1000, n_betas=450, ):
+        """
+        Copy-paste of simulated annealing method delivered to us in the
+        exercise. By no means is this a good bit of code. Improve.
+        TODO:
+          [x] rename parameters so it is actually readable
+          [ ] implement the estimate_max_energy_diff code (and come up with a
+              better name for this function)
+          [ ] optimise the while loop so increments do not take place inside the
+              loop (enumerate, itertools?)
+          [ ] run and test.
+        :param neighbourhood:
+        :param length_markov_chain:
+        :param n_betas:
+        :return:
+        """
+        # beta_list = 1.01 * np.logspace(1, n_betas, base=beta_init)
+
+        initial_energy = self.ising_energy()
+        mean_energies = np.zeros(n_betas)  # Stores the mean energy at each beta
+        stdev_energies = np.zeros(n_betas)  # Stores std energy at each beta
+
+        # Estimate the maximum dE for a certain spin flip
+        max_de = self.estimate_max_energy_diff(neighbourhood)
+        beta_init = 1 / max_de  # sets initial temperature
+
+        factor = 1.05  # increment of beta at each new chain
+
+        t_count = 0
+        stdev_energies[t_count] = 1
+        beta = beta_init
+        while stdev_energies[t_count] > 0:
+            # increment
+            t_count += 1
+            beta *= factor
+            # Initialise energy_array
+            energy_array = np.zeros(length_markov_chain)
+
+            for t1 in length_markov_chain:
+                flip_combinations = self.generate_flip_combinations(
+                    neighbourhood)
+                # Choose new x by flipping to new neighbourhood
+                # Perform metropolis hastings step
+
+                # E1 is energy of new state
+                energy_array[t1] = initial_energy
+
+            mean_energies[t_count] = np.mean(energy_array)
+            stdev_energies[t_count] = np.std(energy_array)
+            print(t_count, beta,
+                  mean_energies[t_count], stdev_energies[t_count])
+
+        return energy_array, mean_energies, stdev_energies
