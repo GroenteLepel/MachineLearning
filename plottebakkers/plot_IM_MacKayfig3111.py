0cb39e65f51d98d06e24519cbfc1f955d37f8af5
GroenteLepel <d.kok@student.science.ru.nl>
2020-01-07 12:18:41 +0100

    Added function to plot the varieties for the final exercise.

--- plottebakkers/plot_IM_MacKayfig3111.py
+++ plottebakkers/plot_IM_MacKayfig3111.py
@@ -22,24 +22,28 @@ def loadpickles():
     return me_fer, stde_fer, temperature_fer, me_fr, stde_fr, temperature_fr
 
 
+def init_plot(nrows, ncols):
+    plt.rcParams.update({'font.size': 17})
+    fig, ax = plt.subplots(nrows, ncols, sharex='all', figsize=(5*ncols, 10))
+    for ax0 in ax[0]:
+        ax0.hlines(y=0, xmin=-10,
+                   xmax=1e4,
+                   colors='grey', linestyles='dotted')
+    return fig, ax
+
+
 def plotfig(temp_fer, std_fer, me_fer,
             temp_fr, std_fr, me_fr):
-    plt.rcParams.update({'font.size': 17})
-    fig, ax = plt.subplots(2, 2, sharex='all', figsize=(10, 10))
-    for ax0 in ax[0]:
-        ax0.hlines(y=0, xmin=temp_fer.min() - 10,
-                   xmax=temp_fer.max() + 100,
-                   colors='grey', linestyles='dotted')
+    fig, ax = init_plot(2, 2)
 
     ax[0][0].set_xscale('log')  # Set x scale of entire plot
     ax[0][0].set_xlim(temp_fr.min(), temp_fr.max())
-    ax[0][0].set_ylim(-7.2, 0.2)
-    ax[0][1].set_ylim(-7.2, 0.2)
+    # ax[0][0].set_ylim(-7.2, 0.2)
+    # ax[0][1].set_ylim(-7.2, 0.2)
 
     ax[0][0].set_ylabel("(Mean) energy")
     ax[0][0].plot(temp_fer, me_fer, c='black')
     ax[0][1].plot(temp_fr, me_fr, c='black')
-    # ax[0][0].yaxis.set_major_formatter(ticker.FormatStrFormatter('%f'))
 
     ax[1][0].set_ylabel(r"$\sigma(E)$")
     ax[1][0].plot(temp_fer, std_fer, c='black')
@@ -53,3 +57,32 @@ def plotfig(temp_fer, std_fer, me_fer,
 
     fig.show()
     # plt.savefig("../data/McKayFig3111.png")
+
+
+def three_columns(x_1, y_11, y_12,
+                  x_2, y_21, y_22,
+                  x_3, y_31, y_32):
+    fig, ax = init_plot(2, 3)
+
+    ax[0][0].set_xscale('log')  # Set x scale of entire plot
+    ax[0][0].set_xlim(x_3.min(), x_3.max())
+    # ax[0][0].set_ylim(-7.2, 0.2)
+    # ax[0][1].set_ylim(-7.2, 0.2)
+
+    ax[0][0].set_ylabel("(Mean) energy")
+    ax[0][0].plot(x_1, y_11, c='black')
+    ax[0][1].plot(x_2, y_21, c='black')
+    ax[0][2].plot(x_3, y_31, c='black')
+
+    ax[1][0].set_ylabel(r"$\sigma(E)$")
+    ax[1][0].plot(x_1, y_12, c='black')
+    ax[1][1].plot(x_2, y_22, c='black')
+    ax[1][2].plot(x_3, y_32, c='black')
+
+    # Set x labels underneath and titles above
+    ax[0][0].set_title("1000")
+    ax[0][1].set_title("2000")
+    ax[0][2].set_title("3000")
+    ax[1][1].set_xlabel("Temperature")
+
+    fig.show()
