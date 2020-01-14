3ee2368872c7c7a868c88bfe7e11803bca8d7b3a
Daniël Kok <d.kok@student.science.ru.nl>
2020-01-14 15:48:37 +0100

    Changed init_plot() to have some more to init, added plot_sa() function to plot 1 optimised model.

--- plottebakkers/plot_IM_MacKayfig3111.py
+++ plottebakkers/plot_IM_MacKayfig3111.py
@@ -24,14 +24,31 @@ def loadpickles():
     return me_fer, stde_fer, temperature_fer, me_fr, stde_fr, temperature_fr
 
 
-def init_plot(nrows, ncols):
+def init_plot(nrows: int, ncols: int):
     plt.rcParams.update({'font.size': 17})
-    fig, ax = plt.subplots(nrows, ncols, sharex='all', figsize=(5*ncols, 10))
+    fig, ax = plt.subplots(nrows, ncols, sharex='all', figsize=(5 * ncols, 10))
 
-    for ax0 in ax[0]:
-        ax0.hlines(y=0, xmin=-10,
-                   xmax=1e4,
-                   colors='grey', linestyles='dotted')
+    if ncols == 1:
+        ax[0].set_xscale('log')
+        ax[0].hlines(y=0, xmin=-10,
+                     xmax=1e4,
+                     colors='grey', linestyles='dotted')
+        ax[nrows - 1].set_xlabel("Temperature")
+
+        ax[0].set_ylabel("(Mean) energy")
+        ax[1].set_ylabel(r"$\sigma(E)$")
+    else:
+        ax[0][0].set_xscale('log')  # Set x scale of entire plot
+        for ax0 in ax[0]:
+            ax0.hlines(y=0, xmin=-10,
+                       xmax=1e4,
+                       colors='grey', linestyles='dotted')
+        for axn in ax[:][nrows - 1]:
+            axn.set_xlabel("Temperature")
+
+        ax[0][0].set_ylabel("(Mean) energy")
+        ax[1][0].set_ylabel(r"$\sigma(E)$")
+
     return fig, ax
 
 
@@ -40,7 +57,6 @@ def plot_mckay():
 
     fig, ax = init_plot(2, 2)
 
-    ax[0][0].set_xscale('log')  # Set x scale of entire plot
     ax[0][0].set_xlim(temp_fr.min(), temp_fr.max())
     # ax[0][0].set_ylim(-7.2, 0.2)
     # ax[0][1].set_ylim(-7.2, 0.2)
@@ -59,8 +75,30 @@ def plot_mckay():
     ax[1][0].set_xlabel("Temperature")
     ax[1][1].set_xlabel("Temperature")
 
-    fig.show()
-    # plt.savefig("../data/McKayFig3111.png")
+    # fig.show()
+    plt.savefig("../data/McKayFig3111.png")
+
+
+def plot_sa(t_values, mean_energies, std_energies, show:  bool = True):
+    fig, ax = init_plot(2, 1)
+
+    ax[0].plot(t_values, mean_energies, c="black")
+    ax[1].plot(t_values, std_energies, c="black")
+
+    ax[0].set_xlim(t_values.min(), t_values.max())
+
+    fig.tight_layout()
+
+    if show:
+        fig.show()
+    else:
+        datafolder = "../data/"
+        fname = "sa_plot.png"
+        plt.draw()
+        # for axn in ax:
+        #     axn.apply_aspect()
+        fig.savefig("{}{}".format(datafolder, fname))
+        print("Saved figure at {}{}.".format(datafolder, fname))
 
 
 def plot_paramtest(method: str):
@@ -73,18 +111,12 @@ def plot_paramtest(method: str):
 
     three_columns(temps, mes, stds, method, show=False)
 
-def three_columns(x_data, y1_data, y2_data, method: str, show = True):
+
+def three_columns(x_data, y1_data, y2_data, method: str, show=True):
     fig, ax = init_plot(2, 3)
 
-    ax[0][0].set_xscale('log')  # Set x scale of entire plot
     ax[0][0].set_xlim(x_data[2].min(), x_data[2].max())
-    # ax[0][0].set_ylim(-7.2, 0.2)
-    # ax[0][1].set_ylim(-7.2, 0.2)
 
-    ax[0][0].set_ylabel("(Mean) energy")
-    ax[1][0].set_ylabel(r"$\sigma(E)$")
-    dy = 0.05
-    dx = 0.0
     for i in range(3):
         ax[0][i].plot(x_data[i], y1_data[i], c='black')
         ax[1][i].plot(x_data[i], y2_data[i], c='black')
@@ -101,9 +133,10 @@ def three_columns(x_data, y1_data, y2_data, method: str, show = True):
             ax[0][_].set_title(params.BETAS[_])
     else:
         raise ValueError("No proper method given.")
-    ax[1][1].set_xlabel("Temperature")
 
     if show:
         fig.show()
     else:
-        plt.savefig("../data/paramtest_{}.png".format(method))
+        path = "../data/paramtest_{}.png".format(method)
+        plt.savefig(path)
+        print("Saved figure at {}.".format(path))
