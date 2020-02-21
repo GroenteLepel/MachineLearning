4f854fda203a5c8d3654fd65f1253ee4591d57de
GroenteLepel <d.kok@student.science.ru.nl>
2020-02-21 09:33:57 +0100

    Created a draw() and show() method so that multiple arms can be drawn into one figure.

--- week_9/arm.py
+++ week_9/arm.py
@@ -1,8 +1,8 @@
 import numpy as np
 from functools import partial
 from scipy.optimize import fsolve
-#from week_9.joint import Joint
-#from joint.py import Joint
+# from week_9.joint import Joint
+# from joint.py import Joint
 import matplotlib.pyplot as plt
 
 
@@ -15,8 +15,8 @@ class Arm:
             self.joint_angle = angles
         self.noise_parameter = noise_parameter
 
-    def _gen_initial_joints(self):
-        return [Joint(0) for _ in self.n_joints]
+    # def _gen_initial_joints(self):
+    #     return [Joint(0) for _ in self.n_joints]
 
     def move_to(self, target, time_window):
         pass
@@ -41,13 +41,18 @@ class Arm:
     def expected_angle(self, alpha, time_window):
         pass
 
-    def show(self):
-        plt.clf()
-        fig = plt.figure(figsize=(4, 4))
-        ax = fig.add_subplot(1, 1, 1)
+    def draw(self, dashed: bool = False):
+        if not plt.get_fignums():
+            fig = plt.figure(figsize=(4, 4))
+            ax = fig.add_subplot(1, 1, 1)
+        else:
+            fig = plt.gcf()
+            ax = plt.gca()
+
         x_individual = np.cos(self.joint_angle)
         y_individual = np.sin(self.joint_angle)
-        x_joints, y_joints = np.zeros(self.n_joints + 1), np.zeros(self.n_joints + 1)
+        x_joints, y_joints = np.zeros(self.n_joints + 1), np.zeros(
+            self.n_joints + 1)
         for i in range(1, self.n_joints + 1):
             x_joints[i] = x_individual[:i].sum()
             y_joints[i] = y_individual[:i].sum()
@@ -58,5 +63,11 @@ class Arm:
         ax.set_yticks(np.arange(axes_range[0], axes_range[1]))
         ax.set_xlim(axes_range)
         ax.set_ylim(axes_range)
-        ax.plot(x_joints, y_joints, marker='o')
-        fig.show()
+        if dashed:
+            ax.plot(x_joints, y_joints, marker='o', ls='--')
+        else:
+            ax.plot(x_joints, y_joints, marker='o')
+
+    def show(self):
+        plt.show()
+        plt.pause(0.1)
