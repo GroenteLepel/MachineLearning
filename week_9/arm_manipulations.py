54036ff3ffd17a18e358efb070b8fcafa5383c48
GroenteLepel <d.kok@student.science.ru.nl>
2020-02-21 10:16:13 +0100

    Fixed get_functions() to return actual sigma instead of 1/sigma^2, added documentation.

--- week_9/arm_manipulations.py
+++ week_9/arm_manipulations.py
@@ -7,12 +7,18 @@ import copy
 
 
 def get_functions(arm, time_window, time, target, initial_state, alpha=0.1):
+    # separating the x and y from target, and mu and sigma from init
     x_target, y_target = target
     mu, sigma = initial_state[:arm.n_joints], initial_state[arm.n_joints:]
-    eqns = np.zeros(2 * arm.n_joints)  # array containing the equations
+
+    # intialisation of array containing the equations
+    eqns = np.zeros(2 * arm.n_joints)
+
+    # calculating expected x and y for all joints
     x_expected = np.sum(np.cos(mu) * np.exp(- (sigma ** 2) / 2))
     y_expected = np.sum(np.sin(mu) * np.exp(- (sigma ** 2) / 2))
     for i in range(arm.n_joints):
+        # mu equations
         eqns[i] = \
             arm.joint_angle[i] + \
             alpha * (time_window - time) * (
@@ -21,48 +27,69 @@ def get_functions(arm, time_window, time, target, initial_state, alpha=0.1):
                     np.cos(mu[i]) * np.exp(- (sigma[i] ** 2) / 2) *
                     (y_expected - y_target)
             )
-        eqns[arm.n_joints + i] = \
-            1 / arm.noise_parameter * (
-                    1 / (time_window - time) +
-                    alpha * np.exp(- sigma[i] ** 2) -
-                    alpha * (x_expected - x_target) *
-                    np.cos(mu[i]) * np.exp(- (sigma[i] ** 2) / 2) -
-                    alpha * (y_expected - y_target) *
-                    np.sin(mu[i]) * np.exp(- (sigma[i] ** 2) / 2)
+        # sigma equations
+        eqns[arm.n_joints + i] = 1 / np.sqrt(
+                1 / arm.noise_parameter * (
+                        1 / (time_window - time) +
+                        alpha * np.exp(- sigma[i] ** 2) -
+                        alpha * (x_expected - x_target) *
+                        np.cos(mu[i]) * np.exp(- (sigma[i] ** 2) / 2) -
+                        alpha * (y_expected - y_target) *
+                        np.sin(mu[i]) * np.exp(- (sigma[i] ** 2) / 2)
+                )
             )
     return eqns
 
 
 def solve(function, x_init, error_range: float = 0.01):
+    """
+    Solves the given function using fixed point iteration.
+    :param function: function to solve, should only take x_init as parameter.
+    :param x_init: initial point to start the solving method from.
+    :param error_range: error difference between x_new - x_old for accepting
+    x_new
+    :return: ndarray of shape (x_init,).
+    """
     error = 1
     x_old = x_init
     while error > error_range:
         x_new = function(x_old)
-        error = (x_new - x_old).sum()
+        error = np.abs(x_new - x_old).sum()
         x_old = x_new
     return x_new
 
 
 def control(max_time: float, current_time: float,
             expected_angle, initial_angle):
+    """
+    Control function used to calculate the amount of action to give to the arm
+    for it to move
+    :param max_time:
+    :param current_time:
+    :param expected_angle:
+    :param initial_angle:
+    :return:
+    """
     return (1 / (max_time - current_time)) * (expected_angle - initial_angle)
 
 
 def move_arm(arm: Arm, move_to, max_time: float, n_steps: int):
     time_step = max_time / n_steps
     times = np.arange(max_time, step=time_step)
-    init = np.random.normal(size=arm.n_joints * 2)
+    init = np.random.normal(size=arm.n_joints * 2, scale=10)
     for t in times:
-        expected_arm = copy.deepcopy(arm)
         to_solve = partial(get_functions, arm, max_time, t, move_to)
         solution = solve(to_solve, init)
-        action = control(max_time, t, solution[:arm.n_joints], arm.joint_angle)
-
-        expected_arm.joint_angle = solution[:expected_arm.n_joints]
+        expected_angles = solution[:arm.n_joints]
+        sigma = solution[arm.n_joints:]
+        action = control(max_time, t, expected_angles, arm.joint_angle)
         arm.increment_angles(action, time_step)
+
+        expected_arm = copy.deepcopy(arm)
+        expected_arm.joint_angle = expected_angles
         expected_arm.draw(dashed=True)
         arm.draw()
         plt.show()
 
-        init = np.concatenate((arm.joint_angle, solution[arm.n_joints:]))
+        init = np.concatenate((arm.joint_angle, sigma))
 
