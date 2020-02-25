1a8da58406fba6fee69676f537ede73903bbf4d4
Daniël Kok <d.kok@student.science.ru.nl>
2020-02-25 14:09:20 +0100

    Cleanup, added thorough documentation.

--- week_9/run_arm.py
+++ week_9/run_arm.py
@@ -2,7 +2,12 @@ import week_9.arm_manipulations as am
 from week_9.arm import Arm
 import matplotlib.pyplot as plt
 
+# construct arm
 my_arm = Arm(3, 0.01)
+
+# move arm to desired coordinate using the move_arm() method
 am.move_arm(my_arm, [-1, -1], 1, 100)
+
+# feedback
 print(my_arm.joint_angle)
 plt.show()
