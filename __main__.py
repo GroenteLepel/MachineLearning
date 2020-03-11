2c32dded06e687c93e4f156a42adcce1dc62858c
Daniël Kok <d.kok@student.science.ru.nl>
2020-03-11 15:37:04 +0100

    Added the rest of the files to run.

--- __main__.py
+++ __main__.py
@@ -4,4 +4,13 @@ current_path = os.getcwd()
 sys.path.extend([current_path])
 
 # for mcmc
-exec(open('week_2/run_mcmc.py').read())
+# exec(open('week_2/run_mcmc.py').read())
+
+# for simulated annealing
+# exec(open('week_3/run_ising.py').read())
+
+# for boltzmann
+# exec(open('week_6/run_boltzmann.py').read())
+
+# for control theory
+# exec(open('week_9/run_arm.py').read())
