c8503cb2637239ec86673ddd67538801fec64bdc
Daniël Kok <d.kok@student.science.ru.nl>
2020-03-11 12:10:23 +0100

    Removed the show().

--- week_9/run_arm.py
+++ week_9/run_arm.py
@@ -3,11 +3,11 @@ from week_9.arm import Arm
 import matplotlib.pyplot as plt
 
 # construct arm
-my_arm = Arm(3, 0.01)
+my_arm = Arm(100, 0.01)
 
 # move arm to desired coordinate using the move_arm() method
 am.move_arm(my_arm, [0, 0], 1, 30)
 
 # feedback
+print('\n', "Final configuration:")
 print(my_arm.joint_angle)
-plt.show()
